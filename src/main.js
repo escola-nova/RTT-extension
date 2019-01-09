cba234000ceddd5328f819174ffd99d3a13a3d8c
Leonardo Di Vittorio <leonardo.divittorio@live.it>
2019-01-09 18:06:15 +0100

    fix: remove listenr

--- src/main.js
+++ src/main.js
@@ -32,104 +32,108 @@ events.authenticate = function authenticate() {
     });
 };
 
-events.enableTracker = function enableTracker() {
-  console.log('========TRACKING ON===========');
-  /**
-   * Update page.
-   * Update the current url when the user reloads, go to link or go back.
-   *
-   * @listens event:tabs.onUpdated
-   *
-   * @param {func} callback When the chancheInfo.status is 'complete' update the url with tab.url.
-   *
-   * @callback
-   ** @param {integer}   tabId       Id of the current tab.
-   ** @param {object}    changeInfo  Shows the status: string of the chancge.
-   ** @param {Tab}       tab         Contains all Tab infos, including url: string.
-   */
-  window.chrome.tabs.onUpdated.addListener((_, changeInfo, tab) => {
-    if (changeInfo.status === 'complete') {
-      sendData({
-        url: tab.url,
-        startTime: Date.now(),
-      });
-    }
-  });
+/**
+ * Update page.
+ * Update the current url when the user reloads, go to link or go back.
+ *
+ * @listens event:tabs.onUpdated
+ *
+ * @param {func} callback When the chancheInfo.status is 'complete' update the url with tab.url.
+ *
+ * @callback
+ ** @param {integer}   tabId       Id of the current tab.
+ ** @param {object}    changeInfo  Shows the status: string of the chancge.
+ ** @param {Tab}       tab         Contains all Tab infos, including url: string.
+ */
+function _onUpdated(_, changeInfo, tab) {
+  if (changeInfo.status === 'complete') {
+    sendData({
+      url: tab.url,
+      startTime: Date.now(),
+    });
+  }
+}
 
+/**
+ * Activated tab.
+ * Update the current url when the user changes tab.
+ *
+ * @listens event:tabs.onActivated
+ *
+ * @param {func} callback Get the current url by the tabId.
+ *
+ * @callback
+ ** @param {Object}  activeInfo Object with the active tab infos.
+ ** @param {integer} tabId      Id of the current tab.
+ ** @param {integer} windowId   Id of the current window.
+ */
+function _onActivated({tabId}) {
   /**
-   * Activated tab.
-   * Update the current url when the user changes tab.
-   *
-   * @listens event:tabs.onActivated
-   *
-   * @param {func} callback Get the current url by the tabId.
+   * Current tab.
+   * @param {integer} tabId tab id.
+   * @param {func} callback Update the current url with the tab.url.
    *
    * @callback
-   ** @param {Object}  activeInfo Object with the active tab infos.
-   ** @param {integer} tabId      Id of the current tab.
-   ** @param {integer} windowId   Id of the current window.
+   ** @param {Tab} tab All tabs info, including url: string.
    */
-  window.chrome.tabs.onActivated.addListener(({tabId}) =>
-    /**
-     * Current tab.
-     * @param {integer} tabId tab id.
-     * @param {func} callback Update the current url with the tab.url.
-     *
-     * @callback
-     ** @param {Tab} tab All tabs info, including url: string.
-     */
-    window.chrome.tabs.get(tabId, tab => {
+  window.chrome.tabs.get(tabId, tab => {
+    sendData({
+      url: tab.url,
+      startTime: Date.now(),
+    });
+  });
+}
+
+/**
+ * Activated Window.
+ * Update the current url when the user changes window.
+ *
+ * @listens event:windows.onFocusChanged
+ *
+ * @param {func} callback Get the current url by the tabId.
+ */
+function _onFocusChanged() {
+  /**
+   * Current tab.
+   * @param {Object} queryInfo Take a deeper look.
+   * @param {func}   callback  If url is the same twice means it's out of focus. Update the current url with the tab.url if tabs[0] true.
+   *
+   * @callback
+   ** @param {Array} tabs Array of tabs with always one Tab.
+   */
+  window.chrome.tabs.query(
+    {active: true, lastFocusedWindow: true},
+    tabs =>
+      tabs[0] &&
       sendData({
-        url: tab.url,
+        url: tabs[0].url,
         startTime: Date.now(),
-      });
-    }),
+      }), // if url is the same twice means it's out of focus.
   );
 
-  /**
-   * Activated Window.
-   * Update the current url when the user changes window.
-   *
-   * @listens event:windows.onFocusChanged
-   *
-   * @param {func} callback Get the current url by the tabId.
-   */
-  window.chrome.windows.onFocusChanged.addListener(() => {
-    /**
-     * Current tab.
-     * @param {Object} queryInfo Take a deeper look.
-     * @param {func}   callback  If url is the same twice means it's out of focus. Update the current url with the tab.url if tabs[0] true.
-     *
-     * @callback
-     ** @param {Array} tabs Array of tabs with always one Tab.
-     */
-    window.chrome.tabs.query(
-      {active: true, lastFocusedWindow: true},
-      tabs =>
-        tabs[0] &&
-        sendData({
-          url: tabs[0].url,
-          startTime: Date.now(),
-        }), // if url is the same twice means it's out of focus.
-    );
-
-    window.chrome.tabs.query(
-      {active: false, lastFocusedWindow: true},
-      tabs =>
-        tabs[0] &&
-        sendData({
-          url: tabs[0].url,
-          startTime: Date.now(),
-        }),
-    ); // if url is the same twice means it's out of focus.
-  });
-};
+  window.chrome.tabs.query(
+    {active: false, lastFocusedWindow: true},
+    tabs =>
+      tabs[0] &&
+      sendData({
+        url: tabs[0].url,
+        startTime: Date.now(),
+      }),
+  ); // if url is the same twice means it's out of focus.
+}
 
 events.disableTracker = function disableTracker() {
   console.log('========TRACKING OFF===========');
-  window.chrome.tabs.onUpdated.removeListener();
-  window.chrome.tabs.onActivated.removeListener();
-  window.chrome.windows.onFocusChanged.removeListener();
+  window.chrome.tabs.onUpdated.removeListener(_onUpdated);
+  window.chrome.tabs.onActivated.removeListener(_onActivated);
+  window.chrome.windows.onFocusChanged.removeListener(_onFocusChanged);
+};
+
+events.enableTracker = function enableTracker() {
+  console.log('========TRACKING ON===========');
+  window.chrome.tabs.onUpdated.addListener(_onUpdated);
+  window.chrome.tabs.onActivated.addListener(_onActivated);
+  window.chrome.windows.onFocusChanged.addListener(_onFocusChanged);
 };
 
 function sendData(record) {
